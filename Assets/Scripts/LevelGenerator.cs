3f68a3ab7329dd05ca3f733d58a7b260fbe1a900
jackiye776 <jackieworkdrive@gmail.com>
2020-09-14 15:21:56 +1000

    Generate map: TOP-LEFT
    - Generated top-left of the map
    - Used cubes as placeholder for visual assets

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    // 14 columns * 15 rows = 210 tiles
+    int[,] levelMap =
+    {
+        {1,2,2,2,2,2,2,2,2,2,2,2,2,7},
+        {2,5,5,5,5,5,5,5,5,5,5,5,5,4},
+        {2,5,3,4,4,3,5,3,4,4,4,3,5,4},
+        {2,6,4,0,0,4,5,4,0,0,0,4,5,4},
+        {2,5,3,4,4,3,5,3,4,4,4,3,5,3},
+        {2,5,5,5,5,5,5,5,5,5,5,5,5,5},
+        {2,5,3,4,4,3,5,3,3,5,3,4,4,4},
+        {2,5,3,4,4,3,5,4,4,5,3,4,4,3},
+        {2,5,5,5,5,5,5,4,4,5,5,5,5,4},
+        {1,2,2,2,2,1,5,4,3,4,4,3,0,4},
+        {0,0,0,0,0,2,5,4,3,4,4,3,0,3},
+        {0,0,0,0,0,2,5,4,4,0,0,0,0,0},
+        {0,0,0,0,0,2,5,4,4,0,3,4,4,0},
+        {2,2,2,2,2,1,5,3,3,0,4,0,0,0},
+        {0,0,0,0,0,0,5,0,0,0,4,0,0,0},
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        for (int row = 0; row < 15; row++) 
+        { 
+            for (int col = 0; col < 14; col++)
+            {
+                Instantiate(cube, new Vector3(row, -col, 0), Quaternion.identity);
+                Debug.Log("Row: " + row + " || Column: " + col + " || Tile Number: " + levelMap[row, col]);
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
