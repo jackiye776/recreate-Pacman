5032288036d90637465fb1d6bf9a2d95f96da56f
jackiye776 <jackieworkdrive@gmail.com>
2020-09-15 23:32:09 +1000

    Top-Left Level Generator Completed
    - Completed rotation of sprites correctly, everything syncs.
    
    To Do: change sprites to my own one and generate top and bottom right, and bottom left map

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -35,7 +35,6 @@ public class LevelGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
         for (int row = 0; row < 15; row++) 
         { 
             for (int col = 0; col < 14; col++)
@@ -45,7 +44,8 @@ public class LevelGenerator : MonoBehaviour
                 {
                     GameObject tile = GameObject.FindWithTag(tileNum + "");
                     GameObject rotateTile = GameObject.Instantiate(tile, new Vector3(col, -row, 0), Quaternion.identity);
-                    int rotNum = 0;
+                    
+                    int rotNum = 0; // store z value for rotating
 
                     // Rotating outside corners
                     if (tileNum == 1)
@@ -58,31 +58,19 @@ public class LevelGenerator : MonoBehaviour
                     }
 
                     // Rotating outside walls
-                    if(tileNum == 2) { 
+                    if (tileNum == 2) { 
                         if(row >= 1 && row <= 8 || row >= 10 && row <= 12)
                             rotateTile.transform.Rotate(0f, 0f, 90f);
                     }
 
                     // Rotating inside corners
-                    if(tileNum == 3)
+                    if (tileNum == 3)
                     {
                         // Rotate inside corners - top-right 
-                        if (row == 2 && (col == 5 || col == 11))
-                        {
-                            rotNum = 270;
-                        }
-                        if (row == 6 && (col == 5 || col == 8))
-                        {
-                            rotNum = 270; 
-                        }
-                        if (row == 7 && col == 13)
-                        {
-                            rotNum = 270; 
-                        }
-                        if (row == 9 && col == 11) 
-                        {
-                            rotNum = 270; 
-                        }
+                        if (row == 2 && (col == 5 || col == 11)) { rotNum = 270; }
+                        if (row == 6 && (col == 5 || col == 8)) { rotNum = 270; }
+                        if (row == 7 && col == 13) { rotNum = 270; }
+                        if (row == 9 && col == 11) { rotNum = 270; }
 
                         // Rotate inside corners - bottom-left
                         if ((row == 4 || row == 10) && (col == 2 || col == 7 || col == 13)) { rotNum = 90; }
@@ -95,17 +83,22 @@ public class LevelGenerator : MonoBehaviour
 
                         rotateTile.transform.Rotate(0f, 0f, rotNum);
 
-
-                        /*if((row == 2 || row == 6 || row == 7 || row == 9) && (col == 5 || col == 8 || col == 11 || col == 13)) { rotInCorner -= 90; }
-                        if((row == 4 || row == 7 || row == 9 || row == 10 || row == 13) && (col == 2 || col == 7 || col == 8 || col == 10 || col == 13)) { rotInCorner += 90; }
-                        
-                        rotInCorner = 0;*/
                     }
 
                     // Rotating inside walls
-                    if(tileNum == 4)
+                    if (tileNum == 4)
                     {
-
+                        if (col == 13 && (row == 2 || row == 9))
+                            rotateTile.transform.Rotate(0f, 0f, 90f);
+                        if (row == 1 || row == 3 || row == 8 || row == 11 || row == 13 || row == 14 && col != 13)
+                        {
+                            rotateTile.transform.Rotate(0f, 0f, 90f);
+                        }    
+                        if ((row == 7 || row == 9 || row == 10 || row == 12) && (col == 7 || col == 8))
+                        {
+                            rotateTile.transform.Rotate(0f, 0f, 90f);
+                        }
+                        
                     }
 
                     Debug.Log("Instantiated: " + tile);
